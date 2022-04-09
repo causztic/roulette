58c980a0682946e3381bf9efc6fe5e61b89abb0f
causztic <limyaojie93@gmail.com>
2022-04-09 12:44:06 +0800

    Add pie slice rendering based on number of people

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,28 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  // Let n be the number of people
+  // t = 360 / n
+  // r = 250
+  // x(t) = r cos(t)
+  // y(t) = r sin(t)
+
+  const interval = (number) => (2 * Math.PI) / number;
+
+  const angles = (number) => {
+    const array = Array.from(Array(number).keys()).map((index) => {
+      return [index * interval(number), (index + 1) * interval(number)]
+    });
+
+    array[array.length - 1][1] = 0;
+    return array;
+  }
+
+  const y = (t) => 250 * Math.cos(t);
+  const x = (t) => 250 * Math.sin(t);
+  const colors = ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"]
+  const people = ['Ben', 'Jerry', 'Sam']
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,57 +34,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <svg height="500" width="500">
+          <defs>
+            <clipPath id="circle">
+              <circle cx="0" cy="0" r="240" />
+            </clipPath>
+          </defs>
+          <g transform="matrix(1 0 0 -1 250 250)">
+            <circle cx="0" cy="0" r="240" stroke="black" fill="white" />
+            {angles(people.length).map((angle, index) => 
+              <>
+                <path 
+                  key={index}
+                  d={`M0 0 L${x(angle[0])} ${y(angle[0])} A250 250 0 0 0 ${x(angle[1])} ${y(angle[1])} Z`}
+                  fill={colors[index]} clipPath="url(#circle)" />
+              </>
+            )}
+          </g>
+        </svg>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
       </footer>
     </div>
   )
