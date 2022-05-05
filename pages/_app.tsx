2954ff68acf7cd8e2b0e0bcb28c80c175e738f47
yaojie <limyaojie93@gmail.com>
2022-05-05 23:13:32 +0800

    Add fork me ribbon (#4)
    * Add fork me ribbon
    
    * Fix wheel resize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
+import ForkMeImage from '../public/forkme.webp';
 import type { AppProps } from 'next/app';
+import Image from 'next/image';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <a href="https://github.com/causztic/roulette" className="absolute right-0" target="_blank" rel="noopener noreferrer">
+        <Image width={149} height={149} src={ForkMeImage} alt="Fork me on GitHub" />
+      </a>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
